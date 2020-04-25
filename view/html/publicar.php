f34813f91d2aaee8b3025c45ba869171c9c21d9c
kose25 <carlosjosepg@ufps.edu.co>
2020-04-25 18:54:13 -0500

    actualizada la vista de publicar feed

--- view/html/publicar.php
+++ view/html/publicar.php
@@ -1,25 +1,20 @@
+<?php include 'partials/header.php' ?>
 
-<!DOCTYPE html>
-<html lang="">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Publicar</title>
-</head>
-<form method="POST" enctype="multipart/form-data">
-    <div class="form-group" id="descripcion-group">
-                        <input class="form-control" type="text" placeholder="Descripcion" name="descripcion" />
-                    </div>
-               
-                    <div class="form-group" id="foto-group">
-                        <input class="form-control" type="file" name="foto"/>
-                    </div>
-                    <div>
-                    <input class="btn btn-primary" type="submit" values="Subir imagen" name="subir"> </input>
- </div>    
-</form>
+<div class="container">
+    <form method="POST" enctype="multipart/form-data">
+        <div class="form-group" id="descripcion-group">
+            <input class="form-control" type="text" placeholder="Descripcion" name="descripcion" />
+        </div>
 
- <?php
+        <div class="form-group" id="foto-group">
+            <input class="form-control" type="file" name="foto" />
+        </div>
+        <div>
+            <input class="btn btn-primary" type="submit" values="Subir imagen" name="subir"> </input>
+        </div>
+    </form>
+
+    <?php
          if (isset($_POST["subir"])){
             require_once '../../controller/Controller.php';
             $foto=$_FILES['foto']['name'];
@@ -33,7 +28,6 @@
             echo $res;
         }
         ?>
-<body>
-    
-</body>
-</html>
+</div>
+
+<?php include 'partials/footer.php'?>
